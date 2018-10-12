c454229311e9c9226c220d12cb6a11018356fee9
AlexJ <Alex.Jon.Jarvis@Gmail.com>
2018-10-12 12:37:18 +1100

    created endpoints for user api requests

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,21 @@
 var UserInstance = require('../models/User');
 
-exports.test = (req, res) => {
-    res.status(200).json({message: 'Not implemented yet!'});
+exports.create = (req, res) => {
+    res.status(200).json({message: 'Create: Not implemented yet!'});
+}
+
+exports.getAll = (req, res) => {
+    res.status(200).json({message: 'GetAll: Not implemented yet!'});
+}
+
+exports.getOne = (req, res) => {
+    res.status(200).json({message: 'GetOne: Not implemented yet!'});
+}
+
+exports.update = (req, res) => {
+    res.status(200).json({message: 'Update: Not implemented yet!'});
+}
+
+exports.delete = (req, res) => {
+    res.status(200).json({message: 'Delete: Not implemented yet!'});
 }
