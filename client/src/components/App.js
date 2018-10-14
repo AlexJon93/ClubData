92615a5bc05a60f62900e4392321f8bbd787bf75
AlexJ <Alex.Jon.Jarvis@Gmail.com>
2018-10-14 20:33:46 +1100

    frontend now gets list of users from db

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,15 +3,15 @@ import MemberList from './MemberList';
 import '../stylesheets/App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <h1>ClubData</h1>
-        <MemberList />
+        <MemberList/>
       </div>
     );
   }
-
 }
 
 export default App;
